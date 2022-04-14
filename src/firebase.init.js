605933173bdcb9a22eac2bb6bb33869dce950a04
UmmeHania <87921374+UmmeHania@users.noreply.github.com>
2022-04-14 09:31:00 +0600

    get started

---
+++ src/firebase.init.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  // ...
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
+
